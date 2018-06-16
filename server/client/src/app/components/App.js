5522cb0c441304a34ead78510838ec0f8ec420b1
Ubuntu Mate <rhpvieira@gmail.com>
2018-06-15 22:27:40 -0300

    Basic libs and wiring

---
+++ server/client/src/app/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				App.html!
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ server/client/src/index.js
@@ -0,0 +1,19 @@
+import "materialize-css/dist/css/materialize.min.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+
+import App from "./app/components/App";
+import reducers from "./app/reducers";
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.querySelector("#root") 
+);
