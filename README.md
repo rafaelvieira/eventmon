e2d37f4848af4b9bcb8e5d6fd7317d2b79d6f2f9
Ubuntu Mate <rhpvieira@gmail.com>
2018-06-16 10:46:22 -0300

    Adding layout and component structure

--- README.md
+++ README.md
@@ -10,16 +10,18 @@ Super simple event notification monitor built with MERN stack.
 
 ### Backend
 
+Option 1
 * Node.js + Express.js
 * MongoDB
 
+Option 2
+* Google Cloud Platform (Serverless)
 
 ### Application folder structure
 
-src/index.js - Application and modules wiring
-src/app - Main application component module
-src/common - Common utilities and components
-src/<feature>/<redux folders or app layers> - Where specific funcionalities goes
-
-<feature> - Set of funcionalities grouped by a directly related theme.
-<redux folders or app layers> - The basic Redux "actions", "components", "reducers" structure plus usual app layers like "services".
+* src/index.js - Application and modules wiring
+* src/app - Main application component module
+* src/common - Common utilities and components
+* src/\<feature\>/\<redux folders or app layers\> - Where specific funcionalities goes
+    * \<feature\> - Set of funcionalities grouped by a directly related theme.
+    * \<redux folders or app layers\> - The basic Redux "actions", "components", "reducers" structure plus usual app layers like "services".
--- server/client/src/app/components/App.js
+++ server/client/src/app/components/App.js
@@ -1,43 +1,119 @@
 import React, { Component } from "react";
+import { BarChart, Bar, LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Legend, Line } from "recharts";
 
-/*import { LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Legend, Line } from "recharts";
+const Header = () => {
+	return (
+		<header class="section">
+			Eventmon - Monitor your events
+		</header>
+	);
+}
 
-const data = [
-	{name: 'Page A', uv: 4000, pv: 2400},
-	{name: 'Page B', uv: 3000, pv: 1398},
-	{name: 'Page C', uv: 2000, pv: 9800},
-	{name: 'Page D', uv: 2780, pv: 3908},
-	{name: 'Page E', uv: 1890, pv: 4800},
-	{name: 'Page F', uv: 2390, pv: 3800},
-	{name: 'Page G', uv: 3490, pv: 4300},
-];*/
+const Footer = () => {
+	return (
+		<footer class="section">
+			Footer
+		</footer>
+	);
+}
+
+const EventsByCategoryChart = () => {
+	const mockData = [
+		{name: 'Fatal', systemA: 4000, systemB: 2400},
+		{name: 'Critical', systemA: 3000, systemB: 1398},
+		{name: 'Important', systemA: 2000, systemB: 9800},
+		{name: 'Info', systemA: 2780, systemB: 3908},
+	];
+
+	return (
+		<div className="chart-left">
+			Events by category
+			<BarChart width={400} height={300} data={mockData}
+					margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+				<CartesianGrid strokeDasharray="3 3"/>
+				<XAxis dataKey="name"/>
+				<YAxis/>
+				<Tooltip/>
+				<Legend />
+				<Bar dataKey="systemB" fill="#8884d8" />
+				<Bar dataKey="systemA" fill="#82ca9d" />
+			</BarChart>
+		</div>
+	);
+}
+
+const CriticalEventsByTimeChart = () => {
+	
+	const mockData = [
+		{name: 'Jan 2018', systemA: 4000, systemB: 2400},
+		{name: 'Feb 2018', systemA: 3000, systemB: 1398},
+		{name: 'Mar 2018', systemA: 2000, systemB: 9800},
+		{name: 'Apr 2018', systemA: 2780, systemB: 3908},
+		{name: 'Mai 2018', systemA: 1890, systemB: 4800},
+		{name: 'Jun 2018', systemA: 2390, systemB: 3800},
+	];
+
+	return (
+		<div className="chart-right">
+			Critical events by time
+			<LineChart width={550} height={300} data={mockData}
+					margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+				<XAxis dataKey="name"/>
+				<YAxis/>
+				<CartesianGrid strokeDasharray="3 3"/>
+				<Tooltip/>
+				<Legend />
+				<Line type="monotone" dataKey="systemB" stroke="#8884d8" activeDot={{r: 8}}/>
+				<Line type="monotone" dataKey="systemA" stroke="#82ca9d" />
+			</LineChart>
+		</div>
+	);
+}
+
+const EventsList = () => {
+	return (
+		<div className="events-list">
+			<input type="text" placeholder="Type something" />
+			<ul>
+				<li>Event 1</li>
+				<li>Event 2</li>
+				<li>Event 3</li>
+				<li>Event 4</li>
+				<li>Event 4</li>
+				<li>Event 5</li>
+			</ul>
+		</div>
+	);
+}
 
 class App extends Component {
 	componentDidMount() {
-		this.props.fetchEventsBy({
-			days: 30
-		});
+		// this.props.fetchEventsBy({
+		// 	days: 30
+		// });
 	}
 
 	render() {
 		return (
-			<div>
-				App.html!
+			<div className="container">
+				<Header />
+				<div class="divider"></div>
+				<section class="section row">
+					<div class="col s5">
+						<EventsByCategoryChart />
+					</div>
+					<div class="col s7">
+						<CriticalEventsByTimeChart />
+					</div>
+				</section>
+				<div class="divider"></div>
+				<section class="section">
+					<EventsList />
+				</section>
+				<Footer />
 			</div>
 		);
 	}
-	/*
-	<LineChart width={600} height={300} data={data}
-						margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-					<XAxis dataKey="name"/>
-					<YAxis/>
-					<CartesianGrid strokeDasharray="3 3"/>
-					<Tooltip/>
-					<Legend />
-					<Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-					<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-				</LineChart>
-	*/
 }
 
 export default App;
