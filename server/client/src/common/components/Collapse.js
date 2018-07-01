96571afade847dceb9f5fdf63744cb63b9961d54
Ubuntu Mate <rhpvieira@gmail.com>
2018-07-01 12:18:42 -0300

    Collapse component with transitions

--- server/client/src/common/components/Collapse.js
+++ server/client/src/common/components/Collapse.js
@@ -1,12 +1,22 @@
 import React from "react";
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; 
 
 const Collapse = ( { isOpened, closedMessage, children } ) => {
 
-    return isOpened ? 
-        [<div key="collapse_opened" style={{ display: isOpened ? "block" : "none" }}>{ children }</div>] :
-        [<div key="collapse_closed" style={{ display: isOpened ? "none" : "block" }}>
-            { closedMessage ? closedMessage : "Click to show" }
-        </div>];
+    return(
+        <ReactCSSTransitionGroup
+            transitionName="react-transition"
+            transitionEnterTimeout={600}
+            transitionLeaveTimeout={200}>
+            {
+                isOpened ? 
+                    [<div key="collapse_opened" style={{ display: isOpened ? "block" : "none" }}>{ children }</div>] :
+                    [<div key="collapse_closed" style={{ display: isOpened ? "none" : "block" }}>
+                        { closedMessage ? closedMessage : "Click to show" }
+                    </div>]
+            }
+        </ReactCSSTransitionGroup>
+    );
 }
 
 export default Collapse;
